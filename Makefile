36ec40a212611891eb6c91488208304f62b96a24
Monty Anderson <monty.linux@gmail.com>
2016-11-30 12:34:36 +0000

    in-progress file support

---
+++ Makefile
@@ -0,0 +1,2 @@
+default:
+	gcc main.c -O2 -o cfuck
--- main.c
+++ main.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-int main() {
+int main(int argc, char *argv[]) {
 	char *source = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.";
 	size_t source_length = strlen(source);
 	size_t i;
+	FILE *in;
 	FILE *out;
 
-	out = popen("gcc -o a.out -xc -", "w");
+	if(argc < 2) {
+		fprintf(stderr, "No input file specified!\n");
+		exit(1);
+	}
+
+	in = fopen(argv[1], "r");
+
+	out = popen("gcc -O3 -o a.out -xc -", "w");
 
 	fprintf(out, "#include <stdio.h>\n#include <inttypes.h>\nint main() {\n");
 	fprintf(out, "\tuint8_t a[300];\n\tint i = 0;\n\n\t");
